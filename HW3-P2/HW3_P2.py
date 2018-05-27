2a0a5d94b11656059618e9b55e8b2f7d77abff8f
Summer Gaasedelen <sgaasedelen@gmail.com>
2018-05-27 14:36:12 -0500

    confirming conneciton to GitHub

--- HW3-P2/HW3_P2.py
+++ HW3-P2/HW3_P2.py
@@ -332,3 +332,5 @@ minTree = mstPrim(inputGraph)
 print ("The edges of the minimum spanning tree, with {} as the root are:".format(inputGraph[0][0].get_data()))
 for v in minTree:
     print (v)
+
+    #confirming GitHub connection
